4f9e5ebbdcbb3ae6db002976637dd7452ff581f1
ddiablou <jekem13936@jo6s.com>
2023-05-14 18:44:47 +0200

    feat: fisheye command

---
+++ apps/bot/src/commands/Images/fisheye.ts
@@ -0,0 +1,59 @@
+import {
+	Command,
+	RegisterSubCommand,
+} from "@kaname-png/plugin-subcommands-advanced";
+import { resolveKey } from "@sapphire/plugin-i18next";
+import { Stopwatch } from "@sapphire/stopwatch";
+import { AttachmentBuilder } from "discord.js";
+import { generateOptimisedName, lastMedia } from "../../lib/util";
+
+@RegisterSubCommand("image", (builder) =>
+	builder
+	  .setName("fisheye")
+	  .setDescription("🐟👁")
+	  .addIntegerOption((o) =>
+		o.setName("radius").setDescription("Radius of the effect").setMinValue(1).setMaxValue(10).setRequired(false)
+	  )
+	  .addAttachmentOption((o) =>
+		o.setName("image").setDescription("Image").setRequired(false)
+	  )
+  )export class UserCommand extends Command {
+	public override async chatInputRun(
+		interaction: Command.ChatInputInteraction<"cached">,
+	) {
+		await interaction.deferReply({ fetchReply: true });
+		const stopwatch = new Stopwatch();
+
+		const radius = interaction.options.getInteger("radius") ?? 2;
+		const image =
+			interaction.options.getAttachment("image") ??
+			(await lastMedia(interaction.channel!));
+
+		if (!image)
+			return interaction.editReply(
+				await resolveKey(interaction.guild, "commands/images:invalidImage"),
+			);
+
+		const output = await this.container.image.decode(
+			image.proxyURL ?? image.url,
+		);
+
+		output.fisheye(radius);
+
+		const { buffer } = await output.encode();
+		const file = new AttachmentBuilder(Buffer.from(buffer), {
+			name: generateOptimisedName("png"),
+		});
+
+		return interaction.editReply({
+			content: (await resolveKey(
+				interaction.guild,
+				"general:stopwatchFinished",
+				{
+					time: stopwatch.stop().toString(),
+				},
+			)) as string,
+			files: [file],
+		});
+	}
+}
