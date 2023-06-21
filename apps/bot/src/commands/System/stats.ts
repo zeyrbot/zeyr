92d32245bdbbaf6da2bfc75eca46a0614f39a5c9
simxnet <jekem13936@jo6s.com>
2023-06-21 11:58:59 +0200

    fix: stats command

--- apps/bot/src/commands/System/stats.ts
+++ apps/bot/src/commands/System/stats.ts
@@ -1,13 +1,15 @@
-import { cdn } from "../../lib/util";
+import { cdn, format, formatBytes } from "../../lib/util";
 import { Colors } from "@discord-factory/colorize";
 import {
 	Command,
 	RegisterSubCommand
 } from "@kaname-png/plugin-subcommands-advanced";
-import { version as sapphire_version } from "@sapphire/framework";
 import { codeBlock, objectEntries } from "@sapphire/utilities";
-import { EmbedBuilder, version as discordjs_version } from "discord.js";
+import { version as discordjs_version, EmbedBuilder } from "discord.js";
+import { version as sapphire_version } from "@sapphire/framework";
+import { version as ts_version } from "typescript";
 import sharp from "sharp";
+import os from "node:os";
 
 @RegisterSubCommand("system", (builder) =>
 	builder
@@ -20,6 +22,8 @@ export class UserCommand extends Command {
 	) {
 		const versions = this.versions;
 		const uptime = this.uptime;
+		const memoryUsage = this.memoryUsage;
+		const host = this.host;
 		const memory = this.memory;
 
 		const stats = new EmbedBuilder()
@@ -49,22 +53,46 @@ export class UserCommand extends Command {
 					inline: true
 				},
 				{
-					name: "Uptime",
+					name: "Bot uptime",
 					value: codeBlock(
-						`${uptime.day}d ${uptime.hour}h ${uptime.minute}m ${uptime.second}s`
+						format(
+							"{}d {}h {}m {}s",
+							uptime.day,
+							uptime.hour,
+							uptime.minute,
+							uptime.second
+						)
 					)
+				},
+				{
+					name: "Host uptime",
+					value: codeBlock(
+						format(
+							"{}d {}h {}m {}s",
+							host.uptime.day,
+							host.uptime.hour,
+							host.uptime.minute,
+							host.uptime.second
+						)
+					)
+				},
+				{
+					name: "Host",
+					value: codeBlock(host.name)
 				},
 				{
 					name: "Memory usage",
 					value: codeBlock(
-						`Heap: ${memory.ramUsed}mb (Total: ${memory.ramTotal}mb)`
+						format(
+							"Heap: {} (Total: {})",
+							memoryUsage.ramUsed,
+							memoryUsage.ramTotal
+						)
 					)
 				},
 				{
-					name: "Build",
-					value: codeBlock(
-						`${process.env.NODE_ENV === "development" ? "dev" : "prod"} build`
-					)
+					name: "Memory",
+					value: codeBlock(format("{}/{}", memory.ramUsed, memory.ramTotal))
 				}
 			);
 
@@ -81,24 +109,51 @@ export class UserCommand extends Command {
 			hour: Math.floor(uptime / 3600) % 24,
 			minute: Math.floor(uptime / 60) % 60,
 			second: Math.floor(uptime % 60)
-		};
+		} as unknown as Record<string, string>;
 	}
 
 	private get versions() {
 		return {
+			node: process.version,
+			typescript: ts_version,
 			sapphire: sapphire_version,
-			"discord.js": discordjs_version,
-			imagescript: "1.2.16",
+			discordjs: discordjs_version,
+			imagescript: "latest",
 			sharp: sharp.versions.sharp
 		};
 	}
 
-	private get memory() {
+	private get memoryUsage() {
 		const usage = process.memoryUsage();
 
 		return {
-			ramTotal: `${(usage.heapTotal / 1048576).toFixed(2)}`,
-			ramUsed: `${(usage.heapUsed / 1048576).toFixed(2)}`
+			ramTotal: formatBytes(usage.heapTotal),
+			ramUsed: formatBytes(usage.heapUsed)
+		};
+	}
+
+	private get memory() {
+		const free = os.freemem();
+		const total = os.totalmem();
+
+		return {
+			ramTotal: formatBytes(total),
+			ramUsed: formatBytes(free)
+		};
+	}
+
+	private get host() {
+		const name = os.hostname();
+		const uptime = os.uptime();
+
+		return {
+			name,
+			uptime: {
+				day: Math.floor(uptime / 86400),
+				hour: Math.floor(uptime / 3600) % 24,
+				minute: Math.floor(uptime / 60) % 60,
+				second: Math.floor(uptime % 60)
+			} as unknown as Record<string, string>
 		};
 	}
 }
